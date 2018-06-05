395c8664db4f7bcde9351b29f490076d8d08b884
infernalslam <infernal-slam@hotmail.com>
2018-06-05 15:40:42 +0700

    fix code

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,17 +1,41 @@
 <template>
   <div>
-    Crad
+    <div class="box-info" :style="color">
+      <p class="has-text-centered has-text-light"> บัตรเครดิต </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Card'
+  name: 'Card',
+  props: {
+    color: String
+  }
 }
 </script>
 
 <style scoped>
-.bg {
-  border: 1px solid yellow;
+.box-info:hover {
+  -webkit-box-shadow: 0 10px 20px -10px rgba(0,0,0,.6);
+  box-shadow: 0 10px 20px -10px rgba(0,0,0,.6);
+  -webkit-transform: translateY(-4px);
+  transform: translateY(-4px);
+}
+.box-info {
+  text-shadow: 1px 2px 4px rgba(0,0,0,.3);
+  border-radius: 5px;
+  border: 0;
+  cursor: pointer;
+  outline: none;
+  background: #eee;
+  color: rgba(0,0,0,.7);
+  -webkit-transition: all .2s ease;
+  transition: all .2s ease;
+  overflow: hidden;
+  position: relative;
+  margin: 5px;
+  width: auto;
+  padding: 80px;
 }
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <navbar-menu></navbar-menu>
-    <card></card>
+    <card color="background: linear-gradient(30deg,#8ae48d,#16be77);"></card>
+    <card color="background: linear-gradient(30deg,#2074ff,#601fff);"></card>
+    <card color="background: linear-gradient(30deg,#ff4657,#d50a65)"></card>
   </div>
 </template>
 
