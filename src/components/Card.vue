bf8566cd8115a598b1e36bdca0a394704a469588
infernalslam <infernal-slam@hotmail.com>
2018-06-05 15:03:51 +0700

    add navber :sparkles:

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg">
+  <div>
     Crad
   </div>
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,27 @@
 <template>
-  <div class="bg">
-    Navbar
+  <div>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        PAY AND KEEP
+      </a>
+      <a role="button" class="navbar-burger" :class="{ 'is-active': navbarBurgerIsActive }" @click="navbarBurgerIsActive = !navbarBurgerIsActive" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+  </nav>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  data () {
+    return {
+      navbarBurgerIsActive: false
+    }
+  }
 }
 </script>
-
-<style scoped>
-.bg {
-  border: 1px solid red;
-}
-</style>
